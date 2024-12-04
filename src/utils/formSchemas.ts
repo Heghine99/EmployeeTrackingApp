d671f97ad508d70261df35d88a57a7f0681100c9
Heghine <heghine.azatyan99@gmail.com>
2024-12-05 00:25:53 +0400

    add trnslate form for filter schemas and map key for android

--- src/utils/formSchemas.ts
+++ src/utils/formSchemas.ts
@@ -1,9 +1,10 @@
+import { t } from "i18n-js";
 import * as Yup from "yup";
 
 export const validationSchema = Yup.object().shape({
-  name: Yup.string().required("ФИО обязательно"),
-  position: Yup.string().required("Должность обязательна"),
+  name: Yup.string().required(t("filter_validation.name")),
+  position: Yup.string().required(t("filter_validation.position")),
   phone: Yup.string()
-    .matches(/^\+?[0-9]{10,14}$/, "Введите корректный номер телефона")
-    .required("Номер телефона обязателен"),
+    .matches(/^\+?[0-9]{10,14}$/, t("filter_validation.phone_correct"))
+    .required(t("filter_validation.phone_required")),
 });
